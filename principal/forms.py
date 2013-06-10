5e4c58c9a67e7339a64d6ad3c2235e940d62ddb8
Elicia <elicia_cor@hotmail.com>
2013-06-10 18:50:38 -0500

    contactenos

--- principal/forms.py
+++ principal/forms.py
@@ -1,3 +1,4 @@
+#encoding:utf-8
 from django import forms
 from principal.models import *
 from django.contrib.auth.models import User
@@ -5,6 +6,9 @@ from django.forms import ModelForm
 
 class ContactoForm(ModelForm):
 	nombre= forms.RegexField(label="Nombres:", max_length=30, regex=r'^[a-zA-Z ]+$', help_text = "Indica tus nombres", error_message = "Solo letras")
+	ciudad= forms.RegexField(label="Ciudad:", max_length=30, regex=r'^[a-zA-Z ]+$', help_text = "Escribe tu ciudad", error_message = "Solo letras")
 	e_mail= forms.EmailField(label="Email", help_text = "Indica tu email",widget=forms.TextInput(attrs={'class': 'required', 'maxlength':75}) )
+	telefono=forms.RegexField(label="Telefóno:", max_length=7, regex=r'^[0-9]+$', help_text = "Ingrese número telefonico", error_message = "Solo numeros")
+
 	class Meta:
 		model = Contactenos
